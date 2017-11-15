a8f8c48e26ef7c7986c540ffee03f0ff7de53823
Joshua B <joshdev@9ci.com>
2017-11-15 00:42:11 -0600

    added audit stamp test

---
+++ grails-app/domain/gpbench/CityAuditTrail.groovy
@@ -0,0 +1,34 @@
+package gpbench
+
+import gorm.AuditStamp
+
+@AuditStamp
+class CityAuditTrail { //implements CityModel{
+	String name
+	String shortCode
+
+	BigDecimal latitude
+	BigDecimal longitude
+
+	Region region
+	Country country
+
+	static belongsTo = [Region, Country]
+
+	static mapping = {
+		//id column: 'id', generator:'gorm.tools.idgen.SpringIdGenerator'
+		cache true
+	}
+
+	static constraints = {
+		name blank: false, nullable: false
+		shortCode blank: false, nullable: false
+		latitude nullable: false, scale: 4, max:90.00
+		longitude nullable: false, scale: 4, max:380.00
+		region nullable: false
+		country nullable: false
+	}
+
+	String toString() { name }
+
+}
