ef30d4a338a8d4bab4a04aef22bec47364c804cb
sudhir nimavat <sudhir@nimavat.me>
2017-11-17 22:41:19 +0530

    make auditstamp fields not null

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -4,14 +4,14 @@ grails {
             enabled = true
             //For a field to be added by the annotation at least on config setting needs to be present for that field.
             createdBy.field = "createdBy"  // createdBy is default
-            createdBy.constraints = "nullable:true,display:false,editable:false,bindable:false"
+            createdBy.constraints = "nullable:false,display:false,editable:false,bindable:false"
             createdDate.field = "createdDate"
-            createdDate.constraints = "nullable:true,display:false,editable:false,bindable:false"
+            createdDate.constraints = "nullable:false,display:false,editable:false,bindable:false"
 
             editedBy.field = "editedBy"  // createdBy is default
-            editedBy.constraints = "nullable:true,display:false,editable:false,bindable:false"
+            editedBy.constraints = "nullable:false,display:false,editable:false,bindable:false"
             editedDate.field = "editedDate"
-            editedDate.constraints = "nullable:true,display:false,editable:false,bindable:false"
+            editedDate.constraints = "nullable:false,display:false,editable:false,bindable:false"
         }
     }
 }
