d2682b3dc715d415c556ed831f87d5a15875434a
Joshua B <joshdev@9ci.com>
2017-11-17 02:09:32 -0600

    added beforeValidate

--- grails-app/domain/gpbench/CityAuditStampManual.groovy
+++ grails-app/domain/gpbench/CityAuditStampManual.groovy
@@ -20,12 +20,28 @@ class CityAuditStampManual {
 		cache true
 	}
 
-	static constraints = {
-		importFrom(CityBaseline)
-	}
+    static constraints = {
+        name blank: false, nullable: false
+        shortCode blank: false, nullable: false
+        latitude nullable: false, scale: 4, max:90.00
+        longitude nullable: false, scale: 4, max:380.00
+        region nullable: false
+        country nullable: false
+
+        dateCreated nullable:false,display:false,editable:false,bindable:false
+        lastUpdated nullable:false,display:false,editable:false,bindable:false
+        dateCreatedUser nullable:false,display:false,editable:false,bindable:false
+        lastUpdatedUser nullable:false,display:false,editable:false,bindable:false
+    }
 
 	String toString() { name }
 
+    def beforeValidate(){
+        dateCreatedUser = dateCreatedUser?:SecUtil.userId
+        lastUpdatedUser = lastUpdatedUser?:SecUtil.userId
+            dateCreated = dateCreated?: new Date()
+        lastUpdated = lastUpdated?: new Date()
+    }
 
 	def beforeInsert() {
 		dateCreatedUser = SecUtil.userId
