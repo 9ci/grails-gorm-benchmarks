12ce7b4f00185356842838055b28c51d84f39663
Joshua B <joshdev@9ci.com>
2017-11-16 19:20:35 -0600

    default to autowire false

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -16,7 +16,7 @@ grails {
     }
 }
 
-grails.gorm.autowire = Boolean.valueOf(System.getProperty("autowire.enabled", "true"))
+grails.gorm.autowire = Boolean.valueOf(System.getProperty("autowire.enabled", "false"))
 
 grails {
     gorm.default.mapping = {
